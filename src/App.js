aaae2881cdeb0482a10dbdc362ace3928d17c53c
MrGUNU <145864129+MrGUNU@users.noreply.github.com>
2024-08-05 20:27:49 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+       <header className="header">
+      <h1>AlgoTradePro</h1>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="hero_section">
+        <div class="hero_text">
+          <div class="hero_text_head">
+            Welcome to AlgoTradePro,
+          </div>
+          <div class="hero_text_body">
+            Your premier platform for algorithmic trading! Create, deploy, and manage trading strategies with ease using our user-friendly interface and robust tools. With live market data integration from TradingView and support for multiple brokers, you can execute trades seamlessly. Enjoy advanced features like backtesting, risk management, and customizable alerts to refine your strategies and maximize returns. Our dedicated customer support is always available to assist you. Join AlgoTradePro today and revolutionize your trading experience. Trade smarter, not harder, with AlgoTradePro.
+          </div>
+        </div>
+      </div></>
+  );
+}
+
+export default App;
